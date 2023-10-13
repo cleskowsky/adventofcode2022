7b8c982f402efe9615a909abd41958ba89992e42
Christian Leskowsky <christian@leskowsky.net>
2023-10-12 23:35:40 -0400

    Added day 1 part 1

---
+++ day1.py
@@ -0,0 +1,13 @@
+import re
+
+a = open("day1.txt").read()
+
+
+def ints(s):
+    return tuple(map(int, re.findall('[0-9]+', s)))
+
+
+elves = [ints(x) for x in a.split("\n\n")]
+print(elves)
+
+print(max(sum(elf) for elf in elves))
